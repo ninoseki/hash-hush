f60559edd80c2160c13fb84c78631c34234f0c1e
Manabu Niseki <manabu.niseki@gmail.com>
2019-10-25 11:48:06 +0900

    feat: add how-to section
    Add how to create a link section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
     <div class="content">
-      <h4>What is it?</h4>
-      <p>hash-hush is a smart url for hashes.</p>
-    </div>
-    <div class="content">
-      <h4>Example</h4>
+      <h3>What is it?</h3>
+      <p>Smart links for hashes.</p>
+
+      <h3>Create a link for a hash</h3>
+      <p>
+        Build a URL which follows this pattern:
+        <code>/hash/#{MD5|SHA1|SHA256|SHA512}</code>
+      </p>
+
+      <h3>Example</h3>
       <p>
         <router-link
           to="/hash/275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f"
